7b147559d12aaab95452c0f4bd505cddf7a5454c
issamajd <majd.issa@fibonacci.ae>
2023-03-28 09:11:51 +0400

    * permission based authorization
    fixes

---
+++ Identity.Application.Contracts/Permissions/IdentityPermissionProvider.cs
@@ -0,0 +1,21 @@
+using DDD.Authorization.Abstractions.Permissions;
+
+namespace DDD.Identity.Permissions;
+
+public class IdentityPermissionProvider : IPermissionProvider
+{
+    public void Provide(IPermissionCollection permissionCollection)
+    {
+        var group = permissionCollection.AddGroup(IdentityPermissions.GroupName, "Identity Customer");
+        var customerPermission = group.AddPermission(IdentityPermissions.Customers.Default, "Identity Customer Read");
+        customerPermission.AddChild(IdentityPermissions.Customers.Create, "Identity Customer Create");
+        customerPermission.AddChild(IdentityPermissions.Customers.Edit, "Identity Customer Edit");
+        customerPermission.AddChild(IdentityPermissions.Customers.Delete, "Identity Customer Delete");
+        
+        var providerPermission = group.AddPermission(IdentityPermissions.Providers.Default, "Identity Provider Read");
+        providerPermission.AddChild(IdentityPermissions.Providers.Create, "Identity Provider Create");
+        providerPermission.AddChild(IdentityPermissions.Providers.Edit, "Identity Provider Edit");
+        providerPermission.AddChild(IdentityPermissions.Providers.Delete, "Identity Provider Read");
+        
+    }
+}
