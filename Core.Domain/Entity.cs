b097670026048fdd009ac44be9aa8006e6717545
issamajd <majd.issa@fibonacci.ae>
2023-04-04 11:09:32 +0400

    move equality from AggregateRoot.csto Entity.cs

--- Core.Domain/Entity.cs
+++ Core.Domain/Entity.cs
@@ -12,4 +12,20 @@ public abstract class Entity<TKey> : Entity
     {
         Id = id;
     }
+    public override bool Equals(object? obj)
+    {
+        //Check for null and compare run-time types.
+        if ((obj == null) || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        var other = (Entity<TKey>)obj;
+        return Id != null && Id.Equals(other.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id != null ? Id.GetHashCode() : base.GetHashCode();
+    }
 }
