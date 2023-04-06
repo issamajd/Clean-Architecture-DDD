5a697f4ec2f9d3dd3fed419ca3e45185a97488ce
issamajd <majd.issa@fibonacci.ae>
2023-04-06 21:38:10 +0400

    Refactoring step 2 Auditing domain

---
+++ Auditing/Twinkle.Auditing.Domain/Twinkle/Auditing/Domain/AuditLogs/EntityChange.cs
@@ -0,0 +1,32 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using Twinkle.SeedWork.Domain;
+using Twinkle.SeedWork.Utils;
+
+namespace Twinkle.Auditing.Domain.AuditLogs;
+
+public class EntityChange : Entity<Guid>
+{
+    [Required] public Guid AuditLogId { get; private set; }
+    [Required] public DateTime ChangeTime { get; private set; }
+    [Required] public EntityChangeType ChangeType { get; private set; }
+    [Required] public Guid EntityId { get; private set; }
+    [Required] public string EntityType { get; private set; }
+    internal ICollection<EntityPropertyChange> EntityPropertyChanges { get; }
+
+    private EntityChange()
+    {
+    }
+
+    internal EntityChange(Guid id, Guid auditLogId, DateTime changeTime, EntityChangeType changeType, Guid entityId,
+        string entityType) : base(id)
+    {
+        AuditLogId = Check.NotNull(auditLogId, nameof(auditLogId));
+        ChangeTime = Check.NotNull(changeTime, nameof(changeTime));
+        ChangeType = Check.NotNull(changeType, nameof(changeType));
+        EntityId = Check.NotNull(entityId, nameof(entityId));
+        EntityType = Check.NotNullOrEmpty(entityType, nameof(entityType));
+
+        EntityPropertyChanges = new Collection<EntityPropertyChange>();
+    }
+}
---
+++ Auditing/Twinkle.Auditing.Domain/Twinkle/Auditing/Domain/AuditLogs/EntityChangeType.cs
@@ -0,0 +1,8 @@
+namespace Twinkle.Auditing.Domain.AuditLogs;
+
+public enum EntityChangeType
+{
+    Created,
+    Updated,
+    Deleted
+}
